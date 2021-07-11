6840016c20dfbcb455e6291c12be73265c50676b
Chris D'Aloisio <chris.daloisio@portable.com.au>
2021-07-11 11:43:30 +1000

    chore: scaffold ExpressJS and React applications

---
+++ server/test/app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+
+const app = require('../src/app');
+
+describe('app', () => {
+  it('responds with a not found message', (done) => {
+    request(app)
+      .get('/what-is-this-even')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
+  });
+});
